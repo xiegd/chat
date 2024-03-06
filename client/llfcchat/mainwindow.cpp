d7f50fb71e65ff7e93b70dd96d234543a3133fe2
wangwuchen <wuchenwang@gaugene.com>
2024-03-06 10:00:12 +0800

    修复mainwindow析构导致二次回收内存的问题。

--- client/llfcchat/mainwindow.cpp
+++ client/llfcchat/mainwindow.cpp
@@ -7,28 +7,22 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     //创建一个CentralWidget, 并将其设置为MainWindow的中心部件
-    _login_dlg = new LoginDialog();
+    _login_dlg = new LoginDialog(this);
     setCentralWidget(_login_dlg);
     _login_dlg->show();
 
     //创建和注册消息的连接
     connect(_login_dlg, &LoginDialog::switchRegister, this, &MainWindow::SlotSwitchReg);
 
-    _reg_dlg = new RegisterDialog();
+    _reg_dlg = new RegisterDialog(this);
+    _reg_dlg->hide();
+    _login_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+     _reg_dlg->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    if(_login_dlg){
-        delete _login_dlg;
-        _login_dlg = nullptr;
-    }
-
-    if(_reg_dlg){
-        delete _reg_dlg;
-        _reg_dlg = nullptr;
-    }
 }
 
 void MainWindow::SlotSwitchReg()
